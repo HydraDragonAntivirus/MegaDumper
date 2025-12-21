5a76c03c5f0a895c6b9b9c3ab83eacd8f26c2526
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2025-12-21 03:03:16 +0300

    Update MegaDumper_Safe.bat

--- MegaDumper/MegaDumper_Safe.bat
+++ MegaDumper/MegaDumper_Safe.bat
@@ -4,6 +4,7 @@ REM This launcher ensures the COMPlus_legacyCorruptedStateExceptionsPolicy is se
 REM before the .NET runtime starts, enabling AccessViolationException to be caught.
 
 set COMPlus_legacyCorruptedStateExceptionsPolicy=1
+set DOTNET_legacyCorruptedStateExceptionsPolicy=1
 
 REM Start MegaDumper
 "%~dp0MegaDumper.exe" %*
